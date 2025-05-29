68daa7621cf60a81e47766994e37f99bcd839de1
BlueDelma <166474517+BlueDelma@users.noreply.github.com>
2025-05-29 23:53:46 +0900

    DB 인증 로직 추가 및 DBUtil 수정

--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -1,15 +1,19 @@
 package util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DBUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/employee_access_db";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/employee_access_db";
     private static final String USER = "project_user";
-    private static final String PASSWORD = "1234"; 
+    private static final String PASSWORD = "1234";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void close(Connection conn, Statement stmt, ResultSet rs) {
+        try { if(rs != null) rs.close(); } catch (Exception ignored) {}
+        try { if(stmt != null) stmt.close(); } catch (Exception ignored) {}
+        try { if(conn != null) conn.close(); } catch (Exception ignored) {}
     }
 }
